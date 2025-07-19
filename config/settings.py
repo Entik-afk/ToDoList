de43761ba71745aaed8d190ef806efc90ba1bb89
Entik-afk <lech.gajdzica@hotmail.com>
2025-07-19 23:36:45 +0200

    setting allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3&j#-vi7*$g@_z%b=)u(78n7jpq05x#joi4p5l*ey1cib88-ps
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
